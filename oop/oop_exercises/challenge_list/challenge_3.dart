07907bdaf683d12e039b9e0bcb37938210aff6f6
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-04-22 15:51:40 -0300

    add examples and test to challenge 3

--- oop/oop_exercises/challenge_list/challenge_3.dart
+++ oop/oop_exercises/challenge_list/challenge_3.dart
@@ -1,9 +1,10 @@
 class Animal {
   String nome;
   int idade;
-  String? cor; 
+  String? cor;
 
-  Animal({required this.nome, required this.idade, this.cor}); //atributos nomeados
+  // Atributos nomeados (named parameters)
+  Animal({required this.nome, required this.idade, this.cor});
 
   void comer() {
     print('O animal ${this.nome} está comendo');
@@ -12,6 +13,13 @@ class Animal {
   void falar() {
     print('O animal ${this.nome} está falando');
   }
+}
 
-  Animal meuCachorro = Animal(nome: 'Hakeem', idade: 5);
+void main() {
+  Animal meuPeixe = Animal(nome: 'Beta', idade: 1); //cor segue opcional
+  Animal meuCachorro = Animal(nome: 'Hakeem', idade: 5, cor: null);
+  Animal meuGato = Animal(nome: 'Lucy', idade: 8, cor: 'Amarelo');
+  print(meuPeixe.cor); //null
+  print(meuCachorro.cor); //null
+  print(meuGato.cor); //Amarelo
 }
