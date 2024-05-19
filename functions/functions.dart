91e2c45d138a454c6c5ef958f01dd4077386f081
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-05-18 21:11:18 -0300

    add functions notes

--- functions/functions.dart
+++ functions/functions.dart
@@ -1,6 +1,40 @@
 /* Functions
 Functions are objects and have a type Function.
 Can be void or have a return type.
+Without the function keyword, but you can specify the return type
 */
+void main() {
+  void explicitVoidReturnType() {
+    print('Im a function');
+  }
 
+  String aFunctionReturning() {
+    return 'Im a function';
+  }
 
+// Arguments
+  int sum(int a, int b) {
+    return a + b;
+  }
+
+// Positional arguments
+  printCustomer(String name, double balance, bool isActive) {
+    print(name);
+    print(balance);
+    print(isActive);
+  }
+
+// Named arguments (all of them or partial)
+// {named arguments are optional by default} if I want it to be mandatory use {required ...}
+// As they are named I can use in any order when Im calling them
+  printClient(String name, double balance, {required bool isActive}) {
+    print(name);
+    print(balance);
+    print(isActive);
+  }
+
+  callPrint() {
+    printCustomer('Jane', 200, false);
+    printClient('Ana', 300.50, isActive: false);
+  }
+}
