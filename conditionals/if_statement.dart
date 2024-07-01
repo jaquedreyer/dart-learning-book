cde528d10c1fc021dfaa099cc1d5430c5a0d8355
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-07-01 15:05:02 -0300

    add if statement notes

---
+++ conditionals/if_statement.dart
@@ -0,0 +1,21 @@
+/*
+Decision-making statements help determine which block of statements to execute 
+based on a given condition or expression (if is TRUE or FALSE).
+*/
+
+void main(){
+// IF STATEMENT
+// Dart supports if statements with optional else clauses. 
+// The condition in parentheses after if must be an expression that evaluates to a boolean
+
+// if statement syntax
+if (condition) {  
+     //statement(s)  
+}  
+
+var age = 16;  
+ if (age>18){  
+    print('You are eligible for voting');  
+ } print('You are not eligible for voting');   
+
+}  
