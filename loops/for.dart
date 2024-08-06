471479ef07993153c6392e19b8fcb27d3403efaa
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-08-06 13:48:13 -0300

    add for in and forEach notes

--- loops/for.dart
+++ loops/for.dart
@@ -1,25 +1,32 @@
 /*
 The for loop is commonly used for iterating a specific number of times.
 It consists of three parts: initialization, a condition, and an increment statement (action). 
+
+PS: an iterable is a collection of elements that can be accessed sequentially.
 */
 
-
 void main() {
-  List<String> alunos = ['Jair', 'Jaqueline', 'Marcos']; 
-print(alunos.length); // 3
-print(alunos[2]); // Marcos
+  List<String> students = ['Jair', 'Jaqueline', 'Marcos'];
+  print(students.length); // 3
+  print(students[2]); // Marcos
 
-
-  for (int i = 0; i < alunos.length; i++) {
-    print(alunos[i]);
+  for (int i = 0; i < students.length; i++) {
+    print(students[i]);
   }
-  
-  // Lists
-  alunos.forEach((aluno) {
+
+  // Sometimes you might not need to know the current iteration counter when iterating over an Iterable type (like List or Set).
+  // In that case, use the for-in loop:
+
+  // for in
+  for (String aluno in students) {
     print(aluno);
+  }
+
+  // forEach()
+  students.forEach((student) {
+    print(student);
   });
 
-  for (String aluno in alunos) {
-    print(aluno);
-  }
+  var collection = [1, 2, 3];
+  collection.forEach(print); // 1 2 3
 }
