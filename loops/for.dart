97636a3c972b72881930c6f2096a838bd1e5cdb9
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-08-06 13:55:30 -0300

    update with loop code example

--- loops/for.dart
+++ loops/for.dart
@@ -14,6 +14,12 @@ void main() {
     print(students[i]);
   }
 
+  int num = 1;  
+    for(num; num<=10; num++)  //for loop to print 1-10 numbers  
+    {  
+        print(num);  
+    }  
+
   // Sometimes you might not need to know the current iteration counter when iterating over an Iterable type (like List or Set).
   // In that case, use the for-in loop:
 
@@ -29,4 +35,26 @@ void main() {
 
   var collection = [1, 2, 3];
   collection.forEach(print); // 1 2 3
+}
+
+  // Another loop example:
+  class Person {
+  String name;
+  int age;
+  Person(this.name, this.age);
+
+  set setAge(int value) => age = value;
+}
+
+void main() {
+  List<Person> people = [Person('Maria', 18), Person('Joãozinho', 7), Person('Valentina', 12)];
+  List<String> under18 = [];
+
+  people.forEach((person) {
+    if (person.age < 18) {
+      under18.add(person.name);
+    }
+  });
+
+  print(under18);
 }
